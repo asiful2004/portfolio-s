eb094bd71087b140a069a0f563d73085144f135f
asiful830 <infinixhoot10s@gmail.com>
2024-09-12 00:10:56 +0600

    updates by asif

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,7 +76,7 @@ sr.reveal('.contact__input',{interval: 200});
 
 
 // TEXT TYPING ANIMATION 
-var string = "i'm Web Designer"
+var string = "i'm Minecraft Server Developer"
 var array = string.split("");
 var timer;
 
